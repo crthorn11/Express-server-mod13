7d7a97fae2e066d0d636fc3486ae6a7889cc5ba6
Cody T <crthorn11@gmail.com>
2024-05-04 08:24:35 -0400

    Restructured folder and files again... Testing to see if they come through

---
+++ README.md
@@ -0,0 +1,11 @@
+# Module13RU
+Includes my challenge for ORM. I will be connecting my front end js, to my backend with express.js and sql.
+
+# Repository link
+https://github.com/crthorn11/Express-server-mod13/
+
+# Deployed live link
+https://crthorn11.github.io/Express-server-mod13/
+
+# Vidoe recording link
+https://github.com/crthorn11/Express-server-mod13/issues/1#issue-2266717305
---
+++ routes/api/category-routes.js
@@ -0,0 +1,93 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // JOIN with locations, using the Trip through table
+      include: [{ model: Location, through: Category, as: 'category-routes' }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  category_id.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+    },
+    {
+      // Gets a book based on the book_id given in the request parameters
+      where: {
+        productTag_id: req.params.productTag_id,
+      },
+    }
+  )
+    .then((updatedProductTag) => {
+      res.json(updatedProductTag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+//
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
